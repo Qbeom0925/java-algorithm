65470680fe5bbe5bdd97a69876c2b1cead16502a
kyubeom <be_man@naver.com>
2022-07-07 18:06:44 +0900

    문자찾기

--- src/String/문자찾기.java
+++ src/String/문자찾기.java
@@ -1,4 +1,36 @@
 package String;
 
+import java.util.Scanner;
+
 public class 문자찾기 {
+
+    public int solution(String str, char t){
+        int answer = 0;
+
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+
+//        System.out.println(str);
+//        for(int i = 0; i<str.length(); i++ ){
+//            if(str.charAt(i)==t) answer++;
+//        }
+
+        for(char x : str.toCharArray()){
+            if (x==t) answer++;
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        문자찾기 T = new 문자찾기();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+
+        char c = kb.next().charAt(0);
+
+        System.out.println(T.solution(str,c));
+    }
+
 }
