f20e4d509441fbadc933877b88abf7e1e45e314e
kyubeom <be_man@naver.com>
2022-07-14 17:32:28 +0900

    중복문자제거(String.indexOf())

---
+++ src/String/중복문자제거.java
@@ -0,0 +1,21 @@
+package String;
+
+import java.util.Scanner;
+
+public class 중복문자제거 {
+
+    public String solution(String str){
+        String answer = "";
+        for(int i=0; i<str.length(); i++){
+            //System.out.println(str.charAt(i)+" "+i+" "+str.indexOf(str.charAt(i)));
+            if(str.indexOf(str.charAt(i))==i) answer+= str.charAt(i);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        중복문자제거 T = new 중복문자제거();
+        Scanner sc = new Scanner(System.in);
+        System.out.println(T.solution(sc.next()));
+    }
+}
