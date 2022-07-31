04d1788a1f5fa169af69fee378170a7c247d9b69
kyubeom <be_man@naver.com>
2022-08-01 00:11:06 +0900

    팰린드롬

---
+++ src/String/팰린드롬.java
@@ -0,0 +1,23 @@
+package String;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class 팰린드롬 {
+
+    public String solution(String str){
+        String answer = "NO";
+        str = str.toUpperCase().replaceAll("[^A-Z]","");
+        String tmp = new StringBuilder(str).reverse().toString();
+        if(str.equals(tmp)) return "YES";
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        팰린드롬 T = new 팰린드롬();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        System.out.println(T.solution(str));
+    }
+}
