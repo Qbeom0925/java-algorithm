b812666c6d97c9e4915423e968617ea83abeef29
kyubeom <be_man@naver.com>
2022-09-16 01:10:16 +0900

    임시반장정하기

---
+++ src/Array/임시반장정하기.java
@@ -0,0 +1,41 @@
+package Array;
+
+import java.util.Scanner;
+
+public class 임시반장정하기 {
+    public int solution(int n, int[][] arr){
+        int answer =0 , max= Integer.MIN_VALUE;
+
+
+        for(int i=1; i<=n; i++) {
+            int cnt = 0;
+            for(int j=1; j<=n; j++) {
+                for(int k=1; k<=5; k++) {
+                    if(arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if(cnt>max) {
+                max=cnt;
+                answer = i;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        임시반장정하기 T = new 임시반장정하기();
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int[][] arr1 = new int[n1+1][6];
+        for(int i=1; i<=5; i++) {
+            for(int j=1; j<=n1; j++) {
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println(T.solution(n1,arr1));
+    }
+}
