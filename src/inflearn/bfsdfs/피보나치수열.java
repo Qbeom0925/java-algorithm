e7c4e7505f976184df848a5d1b397c0f70c32907
kyubeom <be_man@naver.com>
2022-10-10 19:27:21 +0900

    피보나치수열

---
+++ src/inflearn/bfsdfs/피보나치수열.java
@@ -0,0 +1,25 @@
+package inflearn.bfsdfs;
+
+public class 피보나치수열 {
+    static int[] fibo;
+
+    public static void main(String[] args) {
+        int n=45;
+        fibo = new int[n+1];
+        DFS(n);
+        for(int i=1; i<=n; i++) {
+            System.out.print(fibo[i]+" ");
+        }
+
+
+
+    }
+
+    static int DFS(int n) {
+        if(fibo[n]>0) return fibo[n];	//메모리제이션
+        if(n==1) return fibo[n] = 1;
+        else if(n==2) return fibo[n] = 1;
+        else return fibo[n] = DFS(n-2)+DFS(n-1);
+
+    }
+}
