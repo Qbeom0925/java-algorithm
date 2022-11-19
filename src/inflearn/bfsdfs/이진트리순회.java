e425973340368c80dec736b21911a67fb8c6f7ae
kyubeom <be_man@naver.com>
2022-11-19 23:51:08 +0900

    이진트리순회

--- src/inflearn/bfsdfs/이진트리순회.java
+++ src/inflearn/bfsdfs/이진트리순회.java
@@ -14,9 +14,9 @@ public class 이진트리순회 {
     public void DFS(Node root) {
         if(root == null) return;
         else {
-//			System.out.print(root.data+" "); //전위순회
+			System.out.print(root.data+" "); //전위순회
             DFS(root.lt);
-            System.out.print(root.data+" "); //중위순회
+//            System.out.print(root.data+" "); //중위순회
             DFS(root.rt);
 //			System.out.print(root.data+" "); //후위순회
         }
@@ -27,7 +27,7 @@ public class 이진트리순회 {
         tree.root = new Node(1);
         tree.root.lt = new Node(2);
         tree.root.rt = new Node(3);
-        tree.root.lt.rt = new Node(4);
+        tree.root.lt.lt = new Node(4);
         tree.root.lt.rt = new Node(5);
         tree.root.rt.lt = new Node(6);
         tree.root.rt.rt = new Node(7);
