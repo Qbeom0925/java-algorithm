d558b215d65ab623fc5a3ffd2ea85711b9827360
kyubeom <be_man@naver.com>
2022-12-28 23:59:32 +0900

    K번째로큰수

--- src/inflearn/TwoPointers/K번째로큰수.java
+++ src/inflearn/TwoPointers/K번째로큰수.java
@@ -7,13 +7,16 @@ import java.util.TreeSet;
 public class K번째로큰수 {
 
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+        int n;
+        int[] arr;
+        try (Scanner sc = new Scanner(System.in)) {
+            n = sc.nextInt();
+            int k = sc.nextInt();
 
-        int[] arr= new int[n];
-        for(int i=0; i<n; i++) {
-            arr[i] =sc.nextInt();
+            arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
         }
 
         int answer=0;
